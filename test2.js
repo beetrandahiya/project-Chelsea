bdba0853388c4709b9975dab7742ac9e514fae9f
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-02-13 20:12:19 +0530

    hash_code_practice_viz

--- test2.js
+++ test2.js
@@ -4,31 +4,108 @@ setCanvas(elem);
 w = WIDTH;
 h = HEIGHT;
 
+data={0: '9657', 1: '672', 2: '951', 3: '', 4: '8', 5: '0729', 6: '081', 7: '051', 8: '964', 9: '0582'}
 
 
-fetch_mouse_pos(elem, 'mousemove');
-loadFont("BADABB__.TTF", 'badaboom');
 
+data=Object.values(data);
+
+nodeState=[];
+
+data1=[];
+for(i=0;i<data.length;i++){
+   data1.push([])
+   for(j=0;j<data[i].length;j++){
+      data1[i].push(parseInt(data[i][j]));
+      data1[i].sort();
+   }
+   nodeState.push(1);
+}
+//console.log(data1);
+
+
+
+
+/*
+
+for(i=0;i<data1.length;i++){
+
+   for(j=0;j<data1[i].length;j++){
+      k=data1[i][j];
+      a=data1[i].length;
+      b=data1[k].length;
+      console.log(a,b);
+
+      if(a>b){
+         data1[i]=[];
+         nodeState[i]=0;
+         console.log(i);
+         break;
+      }
+      else if(a<b){
+         data1[k]=[];
+         nodeState[k]=0;
+         console.log(i);
+         break;
+      }
+      else if(a==b){
+         continue;
+      }
+      
+   }
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+points1=points(WIDTH/2,HEIGHT/3,100,data.length);
 
 
 function draw() {
 
-   clearCanvas();
+   //clearCanvas();
 
-   // find a random point on a circle
-  let angle1 = random(0, 2 * PI);
-  let xpos1 = 300 + 200 * cos(angle1);
-  let ypos1 = 300 + 200 * sin(angle1);
+   for(i=0;i<points1.length;i++){
+      if(nodeState[i]==1){
+      new point(points1[i].x,points1[i].y,'#fff',5);}
+      new text(points1[i].x+10,points1[i].y,i,20,'Inter',400,'#fff',0,'#695fe7');
+   }
+   for(i=0;i<data1.length;i++){
+      for(j=0;j<data1[i].length;j++){
+         k=data[i][j];
+         new line(points1[i].x,points1[i].y,points1[k].x,points1[k].y,'#695fe7',1);
+       
+   }
+   }
 
-  // find another random point on the circle
-  let angle2 = random(0, 2 * PI);
-  let xpos2 = 300 + 200 * cos(angle2);
-  let ypos2 = 300 + 200 * sin(angle2);
-
-  new line(xpos1,ypos1,xpos2,ypos2,'#fff4');
- requestAnimationFrame(draw);
+// requestAnimationFrame(draw);
    
 }
+
+function points(x,y,radius,sides){
+   points=[];
+   var angle = (Math.PI*2)/sides;
+   for(var i=0; i<sides; i++){
+       var x = x+radius*Math.cos(angle*i);
+       var y = y+radius*Math.sin(angle*i);
+       points.push({x:x,y:y});
+   }
+   return points;
+}
+
 
 
 
