bb69bf89723b7b13a8f93810f416a7d4611ee55d
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-04-12 15:55:44 +0530

    "points in vector field" simulations

--- test2.js
+++ test2.js
@@ -1,4 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////
+/*
 class BinaryTree {
   constructor(value) {
 
@@ -29,13 +30,18 @@ class BinaryTree {
       } else {
         this.data = value
       }
-
     }
-
-  }
-
-  
+  } 
 }
+
+a = new BinaryTree(100)
+a.insert(20)
+a.insert(121)
+a.insert(35)
+a.insert(10)
+
+
+*/
 //////////////////////////////////////////////////////////////////////////
 
 
@@ -46,20 +52,78 @@ w = WIDTH;
 h = HEIGHT;
 
 
-a = new BinaryTree(100)
-a.insert(20)
-a.insert(121)
-a.insert(35)
-a.insert(10)
+n=300;
+poss=[]
+for(ix=0;ix<n;ix++){
+  pos={
+  x:random(0,w),
+  y:random(0,h),
+  vx:0,
+  vy:0,
+  past:[],
+  };
+  poss.push(pos);
+};
 
-
+t=0
+v1=-2*PI;
+v2=2*PI;
 function draw() {
 
-  // clearCanvas();
+   clearCanvas();
+ /*
+   for(i=0;i<w;i+=40){
+    for(j=0;j<h;j+=40){
+      
+      x=mapRange(pos.x,0,w,v1,v2);
+    y=mapRange(pos.y,0,h,v1,v2);
+    g=sin(y)   //vertical component
+    f=cos(x);    //horizonatl componenet 
+    th= atan(f/g);
+      x1=i+5*cos(th);
+      y1=j+5*sin(th);
+      points=[[i,j],[x1,y1]];
+      for(k=0;k<12;k++){
+        y=mapRange(y1,0,h,v1,v2)
+        x=mapRange(x1,0,w,v1,v2)
+        g=sin(y)   //vertical component
+        f=cos(x);    //horizonatl componenet 
+         th= atan(f/g);
+        x2=x1+5*cos(th);
+        y2=y1+5*sin(th);
+        points.push([x2,y2])
+        x1=x2;
+        y1=y2;
+      }
+      ic=mapRange(th,0,2*PI,280,330);
+      ic=`hsla(${ic},100%,50%,0.5)`;
+      new polygon(points,'none',0,ic,1,false);
+      
+    }
+  }
+  
+*/
+  for(i=0;i<n;i++){
+    pos=poss[i]
+    xp=mapRange(pos.x,0,w,v1,v2);
+    yp=mapRange(pos.y,0,h,v1,v2);
+    g=sin(yp);   //horizontal component
+    f=cos(xp);    //vertical component 
+    th= atan(f/g);
+    //console.log(th)
+    ic=mapRange(cos(th),-1,1,230,300);
+    ic=`hsla(${ic},100%,50%,1)`;
+    new point(pos.x,pos.y,ic,2,false);
+    pos.x+=g;
+    pos.y+=f;
+    pos.past.push([pos.x,pos.y]);
+    if(pos.past.length>50){
+      pos.past.shift();
+    }
+    new polygon(pos.past,'none',0,ic,1,false);
+  }
 
-
-
-  // requestAnimationFrame(draw);
+   requestAnimationFrame(draw);
 
 }
 
