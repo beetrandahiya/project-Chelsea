93558b55a1e4be112a8194a57606a701e2039923
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-04-27 22:59:43 +0530

    Update marchin_sq.js

--- marchin_sq.js
+++ marchin_sq.js
@@ -13,11 +13,17 @@ t = 0;
 i = 0;
 
 
-ii = 50;
+ii = 1;
 c = 1;
 
 function eqn(x, y) {
-   return (x * x) + (y * y) - t*t;
+   x=x-w/2;
+   y=y-h/2;
+   x=x/30;
+   y=y/30;
+
+   return y*cos(y+x+x**2)-x**3;
+  // return 8*((x**2) + (y**2))**2 - 25*((x**2)-(y**2));
 }
 
 
@@ -53,30 +59,30 @@ function draw() {
             if (activec[0] == 1) {
                dx = i + (c - d[0]) * ii / (d[1] - d[0]);
                dy = j + (c - d[0]) * ii / (d[2] - d[0]);
-               new line(dx, j, i, dy, `hsl(${t},100%,50%)`, 2);
+               new line(dx, j, i, dy, `hsl(${t},100%,50%)`, 1,"round");
             } else if (activec[1] == 1) {
                dx = i + ((c-d[0])*ii)/(d[1]-d[0]);
                dy = j + ((c-d[1])*ii)/(d[3]-d[1]);
-               new line(dx, j, i+ii, dy, `hsl(${t},100%,50%)`, 2);
+               new line(dx, j, i+ii, dy, `hsl(${t},100%,50%)`, 1,"round");
             } else if (activec[2] == 1) {
                dx = i + ((c-d[2])*ii)/(d[3]-d[2]);
                dy = j + ((c-d[0])*ii)/(d[2]-d[0]);
-               new line(i, dy, dx, j+ii, `hsl(${t},100%,50%)`, 2);
+               new line(i, dy, dx, j+ii, `hsl(${t},100%,50%)`, 1,"round");
             } else if (activec[3] == 1) {
                dx = i + ((c-d[2])*ii)/(d[3]-d[2]);
                dy = j + ((c-d[1])*ii)/(d[3]-d[1]);
-               new line(i+ii, dy,dx,j+ii, `hsl(${t},100%,50%)`, 2);
+               new line(i+ii, dy,dx,j+ii, `hsl(${t},100%,50%)`, 1,"round");
            
             }
          } else if (activeconvers == 2) {
             if ((activec[0] == 1 && activec[1] == 1) || (activec[2] == 1 && activec[3] == 1)) {
                dy1 = j + (c - d[0]) * ii / (d[2] - d[0]);
                dy2 = j + (c - d[1]) * ii / (d[3] - d[1]);
-               new line(i, dy1, i + ii, dy2, `hsl(${t},100%,50%)`, 2);
+               new line(i, dy1, i + ii, dy2, `hsl(${t},100%,50%)`, 1,"round");
             } else if ((activec[0] == 1 && activec[2] == 1) || (activec[1] == 1 && activec[3] == 1)) {
                dx1 = i + (c - d[0]) * ii / (d[1] - d[0]);
                dx2 = i + (c - d[2]) * ii / (d[3] - d[2]);
-               new line(dx1, j, dx2, j + ii, `hsl(${t},100%,50%)`, 2);
+               new line(dx1, j, dx2, j + ii, `hsl(${t},100%,50%)`, 1,"round");
             }
            
 
@@ -85,19 +91,19 @@ function draw() {
             if (activec[0] == 0) {
                dx = i + (c - d[0]) * ii / (d[1] - d[0]);
                dy = j + (c - d[0]) * ii / (d[2] - d[0]);
-               new line(dx, j, i, dy, `hsl(${t},100%,50%)`, 2);
+               new line(dx, j, i, dy, `hsl(${t},100%,50%)`, 1,"round");
             } else if (activec[1] == 0) {
                dx = i + ((c-d[0])*ii)/(d[1]-d[0]);
                dy = j + ((c-d[1])*ii)/(d[3]-d[1]);
-               new line(dx, j, i+ii, dy, `hsl(${t},100%,50%)`, 2);
+               new line(dx, j, i+ii, dy, `hsl(${t},100%,50%)`, 1,"round");
             } else if (activec[2] == 0) {
                dx = i + ((c-d[2])*ii)/(d[3]-d[2]);
                dy = j + ((c-d[0])*ii)/(d[2]-d[0]);
-               new line(i, dy, dx, j+ii, `hsl(${t},100%,50%)`, 2);
+               new line(i, dy, dx, j+ii, `hsl(${t},100%,50%)`, 1,"round");
             } else if (activec[3] == 0) {
                dx = i + ((c-d[2])*ii)/(d[3]-d[2]);
                dy = j + ((c-d[1])*ii)/(d[3]-d[1]);
-               new line(i+ii, dy,dx,j+ii, `hsl(${t},100%,50%)`, 2);
+               new line(i+ii, dy,dx,j+ii, `hsl(${t},100%,50%)`, 1,"round");
            
             }         }
 
@@ -112,7 +118,7 @@ function draw() {
   
 
    t += 0.1;
- requestAnimationFrame(draw);
+ //requestAnimationFrame(draw);
 
 
 }
