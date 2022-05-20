68bae459d60f19231092407f9583079a4ac2ccad
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-05-20 12:16:41 +0530

    raycasting and shi...

--- test2.js
+++ test2.js
@@ -1,99 +1,3 @@
-//ray casting
-
-//boundary
-
-class Boundary {
-  constructor(x1, y1, x2, y2) {
-    this.a = createVector(x1, y1);
-    this.b = createVector(x2, y2);
-  }
-
-  show() {
-    new line(this.a.x, this.a.y, this.b.x, this.b.y, "#f8f", 5);
-  }
-}
-
-
-//ray
-class Ray {
-  constructor(x, y, angle) {
-    this.pos = createVector(x, y);
-    this.angle = angle;
-  }
-  cast(boundary) {
-    var x1 = boundary.a.x;
-    var y1 = boundary.a.y;
-    var x2 = boundary.b.x;
-    var y2 = boundary.b.y;
-
-    var x3 = this.pos.x;
-    var y3 = this.pos.y;
-    var x4 = this.pos.x + cos(this.angle) * 100;
-    var y4 = this.pos.y - sin(this.angle) * 100;
-
-    var den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
-    if (den == 0) return false;
-
-    var t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den;
-    var u = ((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den;
-
-    if (t > 0 && t < 1 && u > 0) {
-      var pt = createVector(x1 + t * (x2 - x1), y1 + t * (y2 - y1));
-      return pt;
-    } else {
-      return false;
-    }
-
-  }
-  show(points,boundaries) {
-    this.points = points;
-    this.boundaries = boundaries;
-    var vecs=[];
-    for(var i = 0; i < this.boundaries.length; i++){
-      var vec1 = this.cast(this.boundaries[i]);
-      if(vec1){
-      vecs.push(vec1);
-      }
-    }
-    //checking if false or not
-    if(vecs==false){
-      this.points.push([this.pos.x- cos(this.angle)*1000,this.pos.y+sin(this.angle)*1000]);
-    }
-    else{
-      //check the nearest wall
-      var nearest = vecs[0];
-      //sort vecs according to magnitude
-      var pos=this.pos;
-      vecs.sort(function(a,b){
-        return magnitude(subtractVec(pos, a))-magnitude(subtractVec(pos, b));
-      });
-      nearest = vecs[0];
-      this.points.push([nearest.x,nearest.y]);
-    }
-
-
-    /*var vec1 = this.cast(b);
-    var vec2 = this.cast(c);
-    if (vec1 && vec2) {
-      if (magnitude(subtractVec(this.pos, vec1)) > magnitude(subtractVec(this.pos, vec2))) {
-        this.points.push([vec2.x, vec2.y]);
-      } else {
-        this.points.push([vec1.x, vec1.y]);
-      }
-
-
-    } else if (vec2) {
-      this.points.push([vec2.x, vec2.y]);
-    } else if (vec1) {
-      this.points.push([vec1.x, vec1.y]);
-
-    } else {
-      this.points.push([this.pos.x - cos(this.angle) * 600, this.pos.y + sin(this.angle) * 600]);
-    }
-    */
-
-  }
-}
 
 
 //////////////////////////////////////////////////
@@ -109,35 +13,19 @@ fetch_mouse_pos(elem, "mousemove");
 t = PI / 2;
 
 
-b = new Boundary(300, 100, 400, 200);
-c = new Boundary(200, 200, 400, 300);
-d = new Boundary(100, 200, 100, 300);
-e = new Boundary(200, 200, 200, 300);
-boundaries = [b,c,d,e];
 function draw() {
 
   clearCanvas();
 
   new Glow(10, "glow");
+  new GaussianBlur(10, "blur");
 
   x = mousepos.x;
   y = mousepos.y;
 
-  //making boundaries
-  for(var i=0;i<boundaries.length;i++){
-    boundaries[i].show();
-  }
+  new circle(x, y, 20, "red",1,"#fff",0,"glow");
 
-  //making light cone
-  points = [];
-  for (i = 0; i <= 2 * PI; i += PI / 500) {
-    r = new Ray(x, y, i);
-    r.show(points,boundaries);
-  }
-
-  //draw light cone
-  new polygon(points, "#fff7", 1, "#aaa", 1, true,"glow");
-
+ 
   requestAnimationFrame(draw);
 
 }
