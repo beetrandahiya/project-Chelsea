a86360feaeb0566bf28ae71e971511b9b7d5bb1b
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-05-22 17:09:48 +0530

    made a better coloring for hilbert curve

--- hilbert_curve.js
+++ hilbert_curve.js
@@ -14,7 +14,7 @@ fetch_mouse_pos(elem, "mousemove");
 //////////////////////////////////////////////////
 // Hilbert curve
 
-order=5;
+order=4;
 N= 2**order;
 total= N * N;
 
@@ -80,14 +80,13 @@ function draw() {
 
   clearCanvas();
 
-  for(i=0; i<t-1; i++){
-    c = lerpHex("#8fa1f7", "#691796",i/t);
-    new line(hilbert_points_array[i][0],hilbert_points_array[i][1],hilbert_points_array[i+1][0],hilbert_points_array[i+1][1],c,10,"round");
-  }
+  new Gradient("horizontal",[["0%","#8fa1f7"],["40%","#8d7fd4"],["100%","#691796"]],"pad","grad");
+  
+  new polygon(hilbert_points_array.slice(0,t),"none",0,"url(#grad)",4 ,false);
+  
   if(t<total){
-   t+=1;
-   requestAnimationFrame(draw);
-  }
+   t+=1;}
+  requestAnimationFrame(draw);
 
 }
 
