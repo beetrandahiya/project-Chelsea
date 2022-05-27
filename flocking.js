b2bbe72b1bb6e602b5db381d54e3d56e0cd78657
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-05-28 00:12:29 +0530

    flocking finally done

--- flocking.js
+++ flocking.js
@@ -14,25 +14,25 @@ MAX_SPEED = 2.5;
 MAX_FORCE = 0.1;
 class Boid {
     constructor() {
-        this.pos = createVector(random(0,w), random(0,h));
-        this.vel = createVector(random(-1,1), random(-1,1));
+        this.pos = createVector(random(0, w), random(0, h));
+        this.vel = createVector(random(-MAX_SPEED, MAX_SPEED), random(-MAX_SPEED, MAX_SPEED));
         this.acc = createVector(0, 0);
         this.color = `hsla(${random(0,360)},100%,50%,1)`;
     }
-    edges(){
-        if(this.pos.x>w){
-            this.pos.x=0;
+    edges() {
+        if (this.pos.x > w) {
+            this.pos.x = 0;
         }
-        if(this.pos.x<0){
-            this.pos.x=w;
+        if (this.pos.x < 0) {
+            this.pos.x = w;
         }
-        if(this.pos.y>h){
-            this.pos.y=0;
+        if (this.pos.y > h) {
+            this.pos.y = 0;
         }
-        if(this.pos.y<0){
-            this.pos.y=h;
+        if (this.pos.y < 0) {
+            this.pos.y = h;
         }
-    
+
     }
 
     /////////////////////////////////
@@ -45,20 +45,20 @@ class Boid {
         for (let other of boids) {
             if (other != this) {
                 //keep the average calculation in percetion range
-                let d=distanceVec(this.pos,other.pos);
+                let d = distanceVec(this.pos, other.pos);
                 if (d < perception) {
-                    steer=addVec(steer,other.vel);
+                    steer = addVec(steer, other.vel);
                     total++;
                 }
             }
 
         }
-        if(total>0){
+        if (total > 0) {
             //steering
-            steer = divideVec(steer,total);
+            steer = divideVec(steer, total);
             steer = setMagnitude(steer, MAX_SPEED);
-            steer = subtractVec(steer,this.vel);
-            steer = limitVec(steer,MAX_FORCE);
+            steer = subtractVec(steer, this.vel);
+            steer = limitVec(steer, MAX_FORCE);
 
         }
         return steer;
@@ -74,20 +74,20 @@ class Boid {
         for (let other of boids) {
             if (other != this) {
                 //keep the average calculation in percetion range
-                let d=distanceVec(this.pos,other.pos);
+                let d = distanceVec(this.pos, other.pos);
                 if (d < perception) {
-                    steer=addVec(steer,other.pos);
+                    steer = addVec(steer, other.pos);
                     total++;
                 }
             }
         }
-        if(total>0){
+        if (total > 0) {
             //steering
-            steer = divideVec(steer,total);
-            steer = subtractVec(steer,this.pos);
+            steer = divideVec(steer, total);
+            steer = subtractVec(steer, this.pos);
             steer = setMagnitude(steer, MAX_SPEED);
-            steer = subtractVec(steer,this.vel);
-            steer = limitVec(steer,MAX_FORCE);
+            steer = subtractVec(steer, this.vel);
+            steer = limitVec(steer, MAX_FORCE);
         }
         return steer;
     }
@@ -102,36 +102,34 @@ class Boid {
         for (let other of boids) {
             if (other != this) {
                 //keep the average calculation in percetion range
-                let d=distanceVec(this.pos,other.pos);
+                let d = distanceVec(this.pos, other.pos);
                 if (d < perception) {
-                    let diff = subtractVec(this.pos,other.pos);
-                    diff = divideVec(diff,d*d);
-                    steer = addVec(steer,diff);
+                    let diff = subtractVec(this.pos, other.pos);
+                    diff = divideVec(diff, d * d);
+                    steer = addVec(steer, diff);
                     total++;
                 }
             }
         }
-        if(total>0){
+        if (total > 0) {
             //steering
-            steer = divideVec(steer,total);
+            steer = divideVec(steer, total);
             steer = setMagnitude(steer, MAX_SPEED);
-            steer = subtractVec(steer,this.vel);
-            steer = limitVec(steer,MAX_FORCE);
+            steer = subtractVec(steer, this.vel);
+            steer = limitVec(steer, MAX_FORCE);
         }
         return steer;
     }
 
 
-    flock(boids){
-        this.acc = addVec(this.acc,this.align(boids));
-        this.acc = addVec(this.acc,this.cohesion(boids));
-        this.acc = addVec(this.acc,this.separation(boids));
+    flock(boids) {
+        this.acc = addVec(this.acc, this.align(boids));
+        this.acc = addVec(this.acc, this.cohesion(boids));
+        this.acc = addVec(this.acc, this.separation(boids));
     }
     show() {
         new circle(this.pos.x, this.pos.y, 4, this.color, 1, "#fff", 0);
-        /*let rot=atan(this.vel.y/this.vel.x);
-        new regpolygon(this.pos.x, this.pos.y,5,3,-rot+PI/6,this.color,1,"#fff",0);*/
-    }
+       }
     update() {
         this.pos = addVec(this.pos, this.vel);
         this.vel = addVec(this.vel, this.acc);
