1632fe63910d1b5a44ef7ef309538fbf2f498374
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2022-05-28 02:45:44 +0530

    verdict about GPUjs with Chelsea
    So, it cannot be done, coz classes and shit. I know the reason, cant write now, gotta sleep

--- gpu_js_test.js
+++ gpu_js_test.js
@@ -5,3 +5,31 @@
 
 const gpu = new GPU();
 
+//write chelsea
+
+gpu.addFunction(mapRange);
+
+elem = document.getElementById("container");
+setCanvas(elem);
+w = WIDTH;
+h = HEIGHT;
+
+
+function draw() {
+
+    clearCanvas();
+
+    for (i = 0; i < w; i += 2) {
+        for (j = 0; j < h; j += 2) {
+
+           
+            new rect(i, j, 2.5, 2.5, '#f00', 1, '#fff', 0, 'top-left', 0);
+
+        }
+    }
+
+}
+
+
+//draw stuff
+draw();
